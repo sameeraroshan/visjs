10b99dbfab34a969119ec10c08bdf7a44ad4f7af
sameera roshan <sameeraroshanuom@gmail.com>
2015-04-05 00:31:47 +0530

    addGroup(String name, Group group)

--- src/main/java/org/vaadin/visjs/networkDiagram/options/Groups.java
+++ src/main/java/org/vaadin/visjs/networkDiagram/options/Groups.java
@@ -1,28 +1,45 @@
 package org.vaadin.visjs.networkDiagram.options;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by roshans on 10/29/14.
  */
 public class Groups {
 
-    private List<Group> groups = new ArrayList<>();
+    private Map<String, Group> groups = new HashMap();
 
+    @Deprecated
     public void addGroup(Group group){
-        groups.add(group);
+        addGroup("default",group);
     }
 
+    public void addGroup(String name, Group group){
+        groups.put(name, group);
+    }
+
+    @Deprecated
     public void removeGroup(Group group){
-        groups.remove(group);
+        groups.remove("default");
+    }
+
+    public void removeGroup(String name){
+        groups.remove(name);
     }
 
     public void clearAll(){
         groups.clear();
     }
 
+    @Deprecated
     public List<Group> getGroups(){
+        return (List<Group>) groups.values();
+    }
+
+    public Map<String,Group> getGroupsMap(){
         return groups;
     }
 
